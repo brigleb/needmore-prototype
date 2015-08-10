824a42bf6a063cbaa7484d5152561d585e6cb37d
Raymond Brigleb <ray@needmoredesigns.com>
2015-08-10 14:47:16 -0700

    Adding assets, JS, ignore

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,40 @@
-(function($){
+(function() {
+	var triggerBttn = document.getElementById( 'trigger-overlay' ),
+		overlay = document.querySelector( 'div.overlay' ),
+		closeBttn = overlay.querySelector( 'button.overlay-close' );
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+		support = { transitions : Modernizr.csstransitions };
 
-	window.PROJECT_NAME = {};
-
-	var SHORT_NAME = window.PROJECT_NAME;
-
-	SHORT_NAME.init = function(){
-		SHORT_NAME.setElements();
-		SHORT_NAME.basics();
+	function toggleOverlay() {
+		if( classie.has( overlay, 'open' ) ) {
+			classie.remove( overlay, 'open' );
+			classie.add( overlay, 'close' );
+			var onEndTransitionFn = function( ev ) {
+				if( support.transitions ) {
+					if( ev.propertyName !== 'visibility' ) return;
+					this.removeEventListener( transEndEventName, onEndTransitionFn );
+				}
+				classie.remove( overlay, 'close' );
+			};
+			if( support.transitions ) {
+				overlay.addEventListener( transEndEventName, onEndTransitionFn );
+			}
+			else {
+				onEndTransitionFn();
+			}
+		}
+		else if( !classie.has( overlay, 'close' ) ) {
+			classie.add( overlay, 'open' );
+		}
 	}
 
-	SHORT_NAME.setElements = function(){
-		SHORT_NAME.elems = {};
-	}
-	
-	SHORT_NAME.basics = function(){
-
-	}
-
-	$(window).load(function() {
-
-	});
-
-
-	$(document).ready(function(){
-		
-		SHORT_NAME.init();
-
-	});//close document ready
-
-})(jQuery)
+	triggerBttn.addEventListener( 'click', toggleOverlay );
+	closeBttn.addEventListener( 'click', toggleOverlay );
+})();
